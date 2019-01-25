33c40c91298f925f874a765bc1691fee07386ce0
Klaus Crusius <klaus.crusius@web.de>
2019-01-25 17:26:59 +0100

    added generator and checks

--- src/universal.jl
+++ src/universal.jl
@@ -1,8 +1,8 @@
 """
     UniversalWrapper(::AbstractMatrix)
 
-UniversalWrapper generalizes the well-known wrappers `oiangular, Symmetric, Adjoint,
-Diagonal, SubArray, ...` in a more flexible way.
+UniversalWrapper generalizes the well-known wrappers `*Triangular, Symmetric, Adjoint,
+Diagonal, ...` in a more flexible way.
 Each wrapper is considered as an operation performed on a subset of the set of square
 matrices, applied to such a matrix (called parent).
 The set of operators with the standard composition form a semi-group.
@@ -34,7 +34,7 @@ otherwise:
 struct UniversalWrapper{Tv,P<:AbstractMatrix{Tv},Up,Di,Lo} <: AbstractMatrix{Tv}
    parent::P
    function UniversalWrapper{Tv,P,Up,Di,Lo}(p::P) where {Tv,P<:AbstractMatrix{Tv},Up,Di,Lo}
-       check_updilo(Up, Di, Lo)
+       check_updilo(Up, Di, Lo) || throw(ArgumentError("Up/Lo = $Up/$Lo invalid"))
        new{Tv,P,Up,Di,Lo}(p)
    end
 end
@@ -44,10 +44,17 @@ function UniversalWrapper{Tv1,P1,Up1,Di1,Lo1}(p::UniversalWrapper{Tv,P,Up2,Di2,L
     UniversalWrapper{Tv,P,Up,Di,Lo}(parent(p))
 end
 
-@inline function universal(Up1, Di1, Lo1, p::UniversalWrapper{Tv,P,Up2,Di2,Lo2}) where {Tv,P,Up2,Di2,Lo2}
+@inline function universal(Up1, Di1, Lo1, ::Type{UniversalWrapper{Tv,P,Up2,Di2,Lo2}}) where {Tv,P,Up2,Di2,Lo2}
     Up, Di, Lo = compose(Tv, Up1, Di1, Lo1, Up2, Di2, Lo2)
-    UniversalWrapper{Tv,P,Up,Di,Lo}(parent(p))
+    UniversalWrapper{Tv,P,Up,Di,Lo}
 end
+@inline universal(Up1, Di1, Lo1, p::UniversalWrapper{Tv,P,Up2,Di2,Lo2}) where {Tv,P,Up2,Di2,Lo2} = universal(Up1, Di1, Lo1, typeof(p))(parent(p))
+
+@inline universal(Up, Di, Lo, a::AbstractMatrix) = universal(Up, Di, Lo, UniversalWrapper(a))
+
+@inline universal(Up, Di, Lo, T::Type) = universal(Up, Di, Lo, UniversalWrapper{T,Matrix{T},1,1,1})
+
+@inline universal(t::Tuple{Int,Int,Int}, x) = universal(t..., x)
 
 transpose(p::UniversalWrapper) = universal(2, 1, 2, p)
 adjoint(p::UniversalWrapper) = universal(-2, -1, -2, p)
@@ -60,13 +67,17 @@ lowersymm(p::UniversalWrapper) = universal(2, 1, 1, p)
 upperherm(p::UniversalWrapper) = universal(1, 2, -2, p)
 lowerherm(p::UniversalWrapper) = universal(-2, 2, 1, p)
 conjugate(p::UniversalWrapper) = universal(-1, -1, -1, p)
+diagonal(p::UniversalWrapper) = universal(0, 1, 0, p)
+Base.one(p::UniversalWrapper) = universal(0, 0, 0, p)
 
 
 for gen in (:transpose, :adjoint, :unituppertria, :unitlowertria, :uppertria, :lowertria,
-            :uppersymm, :lowersymm, :upperherm, :lowerherm)
+            :uppersymm, :lowersymm, :upperherm, :lowerherm, :conjugate)
 
     @eval ($gen)(p::AbstractMatrix) = ($gen)(UniversalWrapper(p))
 end
+
+function_to_wrapper(f::Function, T::Type=Complex) = typeof(f(UniversalWrapper(zeros(T,2,2))))
 
 UniversalWrapper(p::UniversalWrapper) = p
 UniversalWrapper(p::P) where {Tv,P<:AbstractMatrix{Tv}} = UniversalWrapper{Tv,P,1,1,1}(p)
@@ -108,7 +119,8 @@ function Base.getindex(U::UniversalWrapper{Tv,<:Any,Up,Di,Lo}, i::Integer,j::Int
 end
 
 function check_updilo(Up::Integer, Di::Integer, Lo::Integer)
-    Up == 0 || Lo == 0 || Up + Lo != 0 || throw(ArgumentError("Up/Lo = $Up/$Lo invalid"))
+    true
+    # Up == 0 || Lo == 0 || Up + Lo != 0
     # Up == 0 || Lo == 0 || Di != 2 || abs(Up+Lo) == 1 || throw(ArgumentError("Up/Di/Lo = $Up/$Di/$Lo invalid"))
     # Di != 0 || Up == 0 || Lo == 0 || throw(ArgumentError("Up/Di/Lo = $Up/$Di/$Lo invalid"))
 end
@@ -131,7 +143,7 @@ end
 @inline function compose_di(Di1, Di2)
     Di1 ==  1 ? Di2 :
     Di1 == -1 ? (Di2 == 2 ? Di2 : -Di2) :
-    Di1 ==  2 ? Di1 :
+    Di1 ==  2 ? Di2 == 0 ? 0 : Di1 :
     0
 end
 @inline cominv(Up) = ifelse(Up == 0, 0, flipsign(3 - abs(Up), Up))
@@ -146,14 +158,72 @@ compose(::Type, args...) = compose(args...)
     compose_ul(Up1, Up2, Lo2), compose_di(Di1, Di2), compose_ul(Lo1, Lo2, Up2)
 end
 
+import Base.∘
+function ∘(::Type{<:UniversalWrapper{T,S,Up1,Di1,Lo1}},::Type{<:UniversalWrapper{T,S,Up2,Di2,Lo2}}) where {T,S,Up1,Di1,Lo1,T2,Up2,Di2,Lo2}
+
+    UniversalWrapper{T,S,compose(T, Up1, Di1, Lo1, Up2, Di2, Lo2)...}
+end
+
+parameters(::Type{<:UniversalWrapper{<:Any,<:Any,Up,Di,Lo}}) where {Up,Di,Lo} = (Up, Di, Lo)
+
+all_parameters(::Type) = [ (Up, Di, Lo) for Up = -2:2 for Di = -1:2 for Lo = -2:2 if check_updilo(Up, Di, Lo)]
+all_parameters(::Type{<:Real}) = [ (Up, Di, Lo) for Up = 0:2 for Di = 0:1 for Lo = 0:2 if check_updilo(Up, Di, Lo)]
+
+function generate(g::Union{Set,AbstractVector}, T::Type = Complex)
+    g = Set([ x isa Function ? function_to_wrapper(x, T) : x for x in g])
+    res = copy(g)
+    n = 0
+    while length(res) != n
+        n = length(res)
+        res2 = copy(res)
+        for v in res2, u in g
+            uv =  u ∘ v
+            if !(uv in res)
+                # println("$u ∘ $v = $uv")
+                push!(res, uv)
+            end
+        end
+    end
+    collect(res)
+end
+
+function norm_conj(U::Type{<:UniversalWrapper{T,S,Up,Di,Lo}}) where {T,S,Up,Di,Lo}
+   V = universal(-1,-1,-1, U)
+   Up2, Di2, Lo2 = parameters(V)
+   n = count( (Up2, Di2, Lo2) .< 0 ) - count( (Up, Di, Lo) .< 0 )
+   n < 0 ? V : n > 0 ? U :
+   Up > Up2 ? U : Up < Up2 ? V :
+   Lo > Lo2 ? U : Lo < Lo2 ? V :
+   Di > Di2 ? U : Di < Di2 ? V : U
+end
+
+
+check_well_defined() = isempty(not_well_defined())
+function not_well_defined()
+    res = []
+    A = UniversalWrapper([1+1im 2+2im; 3+3im 4+4im])
+    for updilo2 in all_parameters(Complex)
+        U2A = universal(updilo2..., A)
+        MU2A = universal(1, 1, 1, Matrix(U2A))
+        for updilo1 in all_parameters(Complex)
+            if universal(updilo1..., U2A) != universal(updilo1..., MU2A)
+                push!(res, (updilo1, updilo2))
+            end
+        end
+    end
+    res
+end
+
 # check associativity of composition (move to test)
-function check_assoc()
+check_assoc() = isempty(non_associatives()) 
+function non_associatives()
+    res = []
     for Up1 in -2:2, Lo1 in -2:2
         for Up2 in -2:2, Lo2 in -2:2
             for Up3 in -2:2, Lo3 in -2:2
                 if compose(compose(Up1, 0, Lo1, Up2, 0, Lo2)..., Up3, 0, Lo3) !=
                    compose(Up1, 0, Lo1, compose(Up2, 0, Lo2, Up3, 0, Lo3)...)
-                    println("($Up1 0 $Lo1) ($Up2, 0, $Lo2) ($Up3, 0, $Lo3)")
+                    push!(res, ((Up1,0,Lo1), (Up2,0,Lo2), (Up3,0,Lo3)))
                 end
             end
         end
@@ -161,8 +231,22 @@ function check_assoc()
     for Di1 in -1:2, Di2 in -1:2, Di3 in -1:2
         if compose(compose(0, Di1, 0, 0, Di2, 0)..., 0, Di3, 0) !=
            compose(0, Di1, 0, compose(0, Di2, 0, 0, Di3, 0)...)
-            println("(0, $Di1, 0) (0, $Di2, 0) (0, $Di3, 0)")
+           push!(res, ((0,Di1,0), (0,Di2,0), (0,Di3,0)))
         end
     end
+    res
 end
 
+check_conjugate_commutative() = isempty(not_commuting_with_conj())
+function not_commuting_with_conj()
+    res = []
+    c = universal(-1, -1, -1, Complex) 
+    for updilo in all_parameters(Complex)
+        u = universal(updilo, Complex)
+        if u ∘ c != c ∘ u
+            push!(res, u)
+        end
+    end
+    res
+end
+∘
